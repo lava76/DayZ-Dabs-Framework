08c774fd9b0e7ff818ec1b373cd6c1399f7863d2
Tyler Paul <tylerpaul2329@gmail.com>
2021-08-03 12:33:12 -0400

    Added Base64 table and ToBase64 Function

--- DabsFramework/Scripts/3_Game/DabsFramework/Encoding/Encoding.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Encoding/Encoding.c
@@ -4,6 +4,13 @@ class Encoding
 		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"
 	};
 	
+	static const ref TStringArray BASE64_TABLE = {
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+		"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
+		"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+		"w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/",
+	};
+	
     static array<int> GetBytes(string str)
     {
         array<int> result = {};
@@ -58,10 +65,10 @@ class Encoding
 
 		return result;
 	}
-	
+		
     static string FromBytesHex(notnull array<int> data)
     {
-        string hex = "";
+        string hex;
         for (int i = 0; i < data.Count(); i++) {
             hex += Encoding.ToHex(data[i]);
         }
@@ -70,4 +77,18 @@ class Encoding
     }
 	
 	// todo base64 encoding so we can convert Steamid64 to DayZ ID
+	static string ToBase64(string input)
+	{
+		string result;
+		input.TrimInPlace();
+		
+		array<int> byte_array = {};
+		for (int i = 0; i < input.Length(); i++) {
+			byte_array.InsertAt(input[i].Hash(), i);
+		}
+		
+		
+		
+		return result;
+	}
 }
