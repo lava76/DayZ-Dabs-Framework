6c225593f0ecfee2aabd3ed615301c3428e9f5f4
Tyler Paul <tylerpaul2329@gmail.com>
2021-08-02 20:26:40 -0400

    Delete UInt8.c

--- DabsFramework/Scripts/3_Game/DabsFramework/Algorithm/UInt8.c
+++
@@ -1,7 +0,0 @@
-class UInt8 
-{
-    static byte Convert(int value)
-    {
-        return (value & 255); //00000000000000000000000011111111 (only the first 8 bits will retain their value)
-    }
-}
