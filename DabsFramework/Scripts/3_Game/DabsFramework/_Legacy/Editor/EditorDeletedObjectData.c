2dfa0649fe8858a81b8d192d3f436acf44b94233
Tyler Paul <tylerpaul2329@gmail.com>
2024-12-05 17:00:21 -0500

    Optimized deleted object data

--- DabsFramework/Scripts/3_Game/DabsFramework/_Legacy/Editor/EditorDeletedObjectData.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/_Legacy/Editor/EditorDeletedObjectData.c
@@ -4,6 +4,8 @@ static int DeletedObjectId;
 	For future reference, it is possible now with GetDebugName() to obtain the id and p3d of an object safely
 	this would not be a terrible way to ensure deleted objects without a Type dont get improperly removed
 	however we need to backport all old information and handle empty Types from the past
+
+	Future reference here: were gonna use shape name!
 */
 
 
@@ -13,7 +15,11 @@ class EditorDeletedObjectData: SerializableBase
 	[NonSerialized()]
 	int ID;
 	
+	// Depreciated
 	string Type;
+	
+	string ModelName;
+	
 	vector Position;
 	int Flags;
 	
@@ -40,9 +46,12 @@ class EditorDeletedObjectData: SerializableBase
 		data.Type = type;
 		data.Position = position;
 		data.Orientation = orientation;
+	
+		data.WorldObject = data.FindObject();		
+		if (data.WorldObject) {
+			data.ModelName = data.WorldObject.GetShapeName();
+		}
 		
-		// todo handle multiple objects of the same name in a 1m radius
-		data.WorldObject = data.FindObject();
 		return data;
 	}
 	
@@ -54,6 +63,7 @@ class EditorDeletedObjectData: SerializableBase
 		
 		EditorDeletedObjectData data = new EditorDeletedObjectData();
 		data.Type = object.GetType();
+		data.ModelName = object.GetShapeName();
 		data.Position = object.GetWorldPosition();
 		data.WorldObject = object;
 		data.Orientation = object.GetOrientation();
@@ -61,11 +71,21 @@ class EditorDeletedObjectData: SerializableBase
 	}
 	
 	Object FindObject(float radius = 0.1)
-	{				
+	{
 		array<Object> objects = {};
-		GetGame().GetObjectsAtPosition3D(Position, radius, objects, null);
+		GetGame().GetObjectsAtPosition3D(Position, radius, objects, null);			
 		
 		foreach (Object object: objects) {
+			if (!object) {
+				continue;
+			}
+
+			// New way of doing it
+			if (ModelName && ModelName == object.GetShapeName()) {
+				return object;
+			}
+			
+			// Deprecated
 			string type;
 			g_Game.ObjectGetType(object, type);
 			if (type == Type) {
@@ -79,16 +99,27 @@ class EditorDeletedObjectData: SerializableBase
 	
 	override void Write(Serializer serializer, int version)
 	{
-		serializer.Write(Type);
+		if (version <= 5) {
+			serializer.Write(Type);
+		}
+		
 		serializer.Write(Position);
 		serializer.Write(Flags);
+		serializer.Write(ModelName);
 	}
 	
 	override bool Read(Serializer serializer, int version)
 	{
-		serializer.Read(Type);
+		if (version <= 5) {
+			serializer.Read(Type);
+		}
+		
 		serializer.Read(Position);
 		serializer.Read(Flags);
+		
+		if (version >= 6) {
+			serializer.Read(ModelName);
+		}
 		
 		return true;
 	}
